34a789d3e80f21f46ecae9ebebf65845c4136880
youssefelaalem <youssefelaalem82@gmail.com>
2025-04-25 19:14:14 +0300

    Enhance App layout with welcome message and improved styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import Hero_Section from "./components/Hero_Section";
 
 function App() {
   return (
-    <div className="flex bg-gray-50  items-center justify-center h-screen">
+    <div className="flex flex-col bg-gray-50 mt-10 items-center justify-center h-screen">
+      <h1 className="text-4xl text-center font-bold text-gray-900">
+        Welcome to the Future of AI
+      </h1>
       <Hero_Section />
     </div>
   );
